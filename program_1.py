a952977a36030953c8c64a7caeffbad5e77b908d
Caiden-Heinrichs <cjheinrichs@students.unwsp.edu>
2026-03-24 15:17:23 -0500

    Completed program_1.py
    Added exception handling

--- program_1.py
+++ program_1.py
@@ -4,10 +4,13 @@
 
 
 def count_file_lines():
-    with open("names.txt", "r") as names:
-        numberOfNames = len(names.readlines())
+    try:
+        with open("names.txt", "r") as names:
+            numberOfNames = len(names.readlines())
 
-    print(f"There are {numberOfNames} names.")
+        print(f"There are {numberOfNames} names.")
+    except:
+        print("File not found.")
 
 if __name__ == '__main__':
     count_file_lines()
